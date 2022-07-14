0431f7d6e02ef6eba8b0d8d721822a3fc8a2125c
VoVanThanh <thanhvo618@gmail.com>
2022-07-14 15:51:05 +0700

    3.5 -> 4.8

--- cryptochain/blockchain.js
+++ cryptochain/blockchain.js
@@ -20,18 +20,45 @@ class Blockchain {
             return false;
         }
         for (let i = 1; i < chain.length; i++) {
-            const { timestamp, lastHash, hash, data } = chain[i];
+            const { timestamp, lastHash, hash, nonce, difficulty, data } =
+                chain[i];
             const actualLastHash = chain[i - 1].hash;
+            const lastDifficulty = chain[i - 1].difficulty;
+
             if (lastHash !== actualLastHash) {
                 return false;
             }
 
-            const validatedHash = cryptoHash(timestamp, lastHash, data);
+            const validatedHash = cryptoHash(
+                timestamp,
+                lastHash,
+                data,
+                nonce,
+                difficulty
+            );
             if (hash !== validatedHash) {
+                return false;
+            }
+
+            if (Math.abs(lastDifficulty - difficulty) > 1) {
                 return false;
             }
         }
         return true;
+    }
+
+    replaceChain(chain) {
+        if (chain.length <= this.chain.length) {
+            console.error('The incoming chain must be longer');
+            return;
+        }
+
+        if (!Blockchain.isValidChain(chain)) {
+            console.error('The incoming chain must be valid');
+            return;
+        }
+        console.log('replacing chain with', chain);
+        this.chain = chain;
     }
 }
 
